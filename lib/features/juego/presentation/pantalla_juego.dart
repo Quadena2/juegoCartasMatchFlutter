2ca2bb134468282cca9adab4d5142bd8e5196b3e
Quadena <redrawer123@gmail.com>
2025-11-29 23:58:23 -0400

    implementado pantalla de juego con botones y funcionabilidad

---
+++ lib/features/juego/presentation/pantalla_juego.dart
@@ -0,0 +1,96 @@
+import 'package:card_memory_game_three/features/juego/presentation/providers/proveedor_juego.dart';
+import 'package:card_memory_game_three/features/juego/presentation/providers/widgets/barra_estadistica.dart';
+import 'package:card_memory_game_three/features/juego/presentation/providers/widgets/capa_pausa.dart';
+import 'package:card_memory_game_three/features/juego/presentation/providers/widgets/controles_juego.dart';
+import 'package:card_memory_game_three/features/juego/presentation/providers/widgets/dialogos_juego.dart';
+import 'package:card_memory_game_three/features/juego/presentation/providers/widgets/tablero_juego.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PantallaJuego extends StatefulWidget {
+  final int tamanoCuadricula;
+  const PantallaJuego({super.key, required this.tamanoCuadricula});
+
+  @override
+  State<PantallaJuego> createState() => _PantallaJuegoState();
+}
+
+class _PantallaJuegoState extends State<PantallaJuego> {
+  // Banderas locales para dialogos
+  bool _victoriaMostrada = false;
+  bool _derrotaMostrada = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Juego ${widget.tamanoCuadricula}x${widget.tamanoCuadricula}",
+        ),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+      ),
+      body: Consumer<ProveedorJuego>(
+        builder: (context, proveedor, child) {
+          // Logica de dialogos
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            if (!mounted) return;
+
+            if (proveedor.victoria && !_victoriaMostrada) {
+              setState(() => _victoriaMostrada = true);
+              DialogosJuego.mostrarVictoria(context, proveedor.puntaje, () {
+                _resetearBanderas();
+                proveedor.reiniciarJuego();
+              });
+            }
+
+            if (proveedor.juegoTerminado && !_derrotaMostrada) {
+              setState(() => _derrotaMostrada = true);
+              DialogosJuego.mostrarDerrota(context, () {
+                _resetearBanderas();
+                proveedor.reiniciarJuego();
+              });
+            }
+          });
+
+          return Column(
+            children: [
+              // Barra superior
+              BarraEstadistica(proveedor: proveedor),
+
+              // Tablero con pausa
+              Expanded(
+                child: Stack(
+                  children: [
+                    TableroJuego(
+                      tamanoCuadricula: widget.tamanoCuadricula,
+                      proveedor: proveedor,
+                    ),
+                    if (proveedor.enPausa)
+                      CapaPausa(alReanudar: proveedor.alternarPausa),
+                  ],
+                ),
+              ),
+
+              // Controles inferiores
+              ControlesJuego(
+                proveedor: proveedor, 
+                alReiniciar: () => DialogosJuego.confirmarReinicio(context, () {
+                  _resetearBanderas();
+                  proveedor.reiniciarJuego();
+                },),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  void _resetearBanderas(){
+    setState(() {
+      _victoriaMostrada = false;
+      _derrotaMostrada = false;
+    },);
+  }
+}
